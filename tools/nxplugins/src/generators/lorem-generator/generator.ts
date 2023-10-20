5dfb75c95322c4e54c42bae218e2ef0fb5415ef6
Sasha Firsov <suns@firsov.net>
2023-10-19 23:36:14 -0700

    `yarn:lorem` generates 500 files 6-7Kb sources, 2Mb

--- tools/nxplugins/src/generators/lorem-generator/generator.ts
+++ tools/nxplugins/src/generators/lorem-generator/generator.ts
@@ -1,11 +1,12 @@
 import {
-    addProjectConfiguration,
     formatFiles,
     generateFiles,
     getProjects,
     Tree,
 } from '@nx/devkit';
+import { LoremIpsum } from "lorem-ipsum";
 import * as path from 'path';
+
 import { LoremGeneratorGeneratorSchema } from './schema';
 
 export async function loremGeneratorGenerator(
@@ -15,13 +16,22 @@ export async function loremGeneratorGenerator(
     const projectsMap = getProjects(tree);
     const proj = projectsMap.get(options.project);
     const { sourceRoot } = proj;
-
+    const lorem = new LoremIpsum({
+        sentencesPerParagraph: {
+            max: 8,
+            min: 4
+        },
+        wordsPerSentence: {
+            max: 16,
+            min: 4
+        }
+    });
     const generateLayer = (parentName = options.name, depth=0, srcPath='') => {
         if( depth>2)
             return
         for (const i0 of [0, 1, 2]) {
             const name= `${parentName}_${i0}`;
-            const text = options.text || "TBD GENERATED";
+            const text = options.text || lorem.generateParagraphs(16);
             generateFiles(tree, path.join(__dirname, 'files'), sourceRoot+'/lib'+srcPath, {
                 ...options,
                 name,
