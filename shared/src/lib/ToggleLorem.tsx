9a317a4b7275e50578752c2b7c35e1767eea69a9
Sasha Firsov <suns@firsov.net>
2024-07-21 22:36:29 -0700

    fix dynamic load

--- shared/src/lib/ToggleLorem.tsx
+++ shared/src/lib/ToggleLorem.tsx
@@ -1,23 +1,35 @@
-import { ReactNode, Suspense, useState } from 'react';
+import { ReactNode, Suspense, useEffect, useState } from 'react';
 import useFetch from 'use-http';
 import Loading from './loading/loading';
+import { set } from 'lodash-es';
 
 /* eslint-disable-next-line */
 export interface ToggleLoremProps {
     moduleUrl: string;
-    scope: string;
+    scope: 'odd' | 'even' | 'shared';
     children: ReactNode;
 }
+
+type dirJsonType = {
+    LOREM_DEPTH: string;
+    LOREM_PARAGRAPHS: string;
+    files: number;
+    totalSize: number;
+    average: number;
+    modules: string[];
+};
 
 export function ToggleLorem(props: ToggleLoremProps) {
     const [shownL, setShownL] = useState(false);
     const [clickedTime, setClickedTime] = useState(0);
     const [checked, setChecked] = useState(false);
-    const { data = {} } = useFetch(
-        new URL(`dir-${props.scope}.json`, props.moduleUrl).toString(),
-        { suspense: true },
-        []
-    );
+    const [data, setData] = useState<dirJsonType>();
+    useEffect(() => {
+        const url = new URL(`dir-${props.scope}.json`, props.moduleUrl).toString();
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((d) => setData(d));
+    }, [props.scope]);
 
     const RenderedTime = () => {
         const ms = Date.now() - clickedTime;
@@ -26,42 +38,32 @@ export function ToggleLorem(props: ToggleLoremProps) {
     return (
         <div>
             <label>
-                <input
-                    type="checkbox"
-                    checked={checked}
-                    onChange={() => setChecked(!checked)}
-                />{' '}
-                use reverse order preload{' '}
+                <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} /> use reverse order
+                preload{' '}
             </label>
             <a
                 onClick={async () => {
-                    checked && console.log('loading', data.modules);
-                    const promiseArr = data.modules.map(
-                        (mod: string) =>
-                            import(new URL(mod, props.moduleUrl).toString())
+                    checked && console.log('loading', data!.modules);
+                    const promiseArr = data!.modules.map(
+                        (mod: string) => import(new URL(mod, props.moduleUrl).toString())
                     );
                     setShownL(!shownL);
                     const startTime = Date.now();
                     setClickedTime(startTime);
                     checked && (await Promise.all(promiseArr));
-                    checked &&
-                        console.log(
-                            'JS loaded in ',
-                            (Date.now() - startTime).toLocaleString()
-                        );
+                    checked && console.log('JS loaded in ', (Date.now() - startTime).toLocaleString());
                 }}
                 className="list-item-link"
             >
                 <h3>Toggle Lorem tree</h3> &nbsp; <i>to test the load timing</i>
                 <br />
-                &nbsp;
-                <code>
-                    • {data.files?.toLocaleString()} files •{' '}
-                    {data.totalSize?.toLocaleString()} bytes •{' '}
-                    <b>{data.average?.toLocaleString()}</b> average •{' '}
-                    {data.LOREM_DEPTH} LOREM_DEPTH • {data.LOREM_PARAGRAPHS}{' '}
-                    LOREM_PARAGRAPHS
-                </code>
+                {data && (
+                    <code>
+                        • {data.files?.toLocaleString()} files • {data.totalSize?.toLocaleString()} bytes •{' '}
+                        <b>{data.average?.toLocaleString()}</b> average • {data.LOREM_DEPTH} LOREM_DEPTH •{' '}
+                        {data.LOREM_PARAGRAPHS} LOREM_PARAGRAPHS
+                    </code>
+                )}
             </a>
             {shownL && (
                 <Suspense fallback={<Loading />}>
